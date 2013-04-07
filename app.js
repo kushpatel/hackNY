074b902e793b14a01687db99a357d7bab271dfda
ub12 <ub12@ub12-VirtualBox.(none)>
2013-04-06 20:32:13 -0500

    json requests

--- app.js
+++ app.js
@@ -4,11 +4,16 @@ var url = require('url');
 //var express = require('express');
 //var app = express();
 
+
+
+
+
 //app.use(express.bodyParser());
 var body = { "message": "hellooo"};
 http.createServer(function(request, response) {
-  var pathname = url.parse(request.url).pathname;
-  if (pathname === "/home")
+  var pathname = url.parse(request.url).pathname.substring(1);
+
+  if (pathname === "home")
   {
   //console.log(pathname);
 	  response.writeHead(200, {"Content-Type": "text/html"});
@@ -18,8 +23,37 @@ http.createServer(function(request, response) {
   else
   {
       response.writeHead(200, {"Content-Type": "application/json"});
-     // response.write("SDF");
-	
-      response.end(JSON.stringify(body));
+var options = {
+ // host: 'www.google.com',
+ host: 'api.nytimes.com',
+  path: '/svc/search/v2/articlesearch.json?' + pathname //'index.html'
+};
+console.log(pathname);
+http.get(options, function(res){
+
+
+    var data = '';
+
+    res.on('data', function (chunk){
+        data += chunk;
+    });
+
+    res.on('error', function(chunk){
+	data += "error";
+	});
+
+    res.on('end',function(){
+	response.write(data);
+	response.end();
+//	console.log(data);
+//        var obj = JSON.parse(data);
+       // console.log( obj.buck.email );
+    })
+//	response.end();
+}); 
   }
+
+	
+   //   response.end(JSON.stringify(body));
+  //}
 }).listen(8888);
