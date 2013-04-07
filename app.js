755c12e2942fe3c389174684f31955f4e638952c
Mainak Ghosh <mainak@mainak-VirtualBox.(none)>
2013-04-07 02:23:26 -0400

    Made charts

--- app.js
+++ app.js
@@ -15,47 +15,86 @@ http.createServer(function(request, response) {
 
   if (pathname === "home")
   {
-  //console.log(pathname);
-	  response.writeHead(200, {"Content-Type": "text/html"});
-	  response.write(fs.readFileSync('index.html'));
-	  response.end();
-  }
+    response.writeHead(200, {"Content-Type": "text/html"});
+    response.write(fs.readFileSync('index.html'));
+    response.end();
+  }   
   else if (pathname.substring(0,4) === "nyt/")
   {
-	pathname = pathname.substring(4);
-console.log(pathname);
-      response.writeHead(200, {"Content-Type": "application/json"});
-var options = {
- // host: 'www.google.com',
- host: 'api.nytimes.com',
-  path: '/svc/search/v2/articlesearch.json?' + pathname //'index.html'
-};
-console.log(pathname);
-http.get(options, function(res){
+   pathname = pathname.substring(4);
+   console.log(pathname);
+   response.writeHead(200, {"Content-Type": "application/json"});
+   var options = {
+     host: 'api.nytimes.com',
+      path: '/svc/search/v2/articlesearch.json?' + pathname //'index.html'
+    }
+    http.get(options, function(res){
 
 
-    var data = '';
+      var data = '';
 
-    res.on('data', function (chunk){
+      res.on('data', function (chunk){
         data += chunk;
-    });
+      });
 
-    res.on('error', function(chunk){
-	data += "error";
-	});
+      res.on('error', function(chunk){
+       data += "error";
+     });
 
-    res.on('end',function(){
-	response.write(data);
-	response.end();
-//	console.log(data);
-//        var obj = JSON.parse(data);
-       // console.log( obj.buck.email );
-    })
-//	response.end();
-}); 
+      res.on('end',function(){
+       response.write(data);
+       response.end();
+     })
+    }); 
   }
+  else if (pathname.substring(0,4) === "myt/")
+  {
+    pathname = pathname.substring(4);
+    var arr = pathname.split("/");
+    console.log(pathname);
+    response.writeHead(200, {"Content-Type": "application/json"});
+    var cb = Array(arr[1]-arr[0]);
+    for (var i = arr[0], j = 0; i < arr[1]; i++, j++) 
+    {
+      var sDate = i+"0101";
+      var eDate = i+"1231";
 
-	
-   //   response.end(JSON.stringify(body));
-  //}
+      cb[j] = { host: 'api.nytimes.com',
+      path: '/svc/search/v2/articlesearch.json?' + "q="+arr[2]+"&begin_date="+sDate+"&end_date="+eDate+"&hl=true&api-key=columbiahack"
+      }
+    }
+    response.write('{"years": [');
+    reck(0);
+
+
+    function reck(k)
+    {
+      if (k==j)
+      {
+        response.write("]}");
+        response.end();
+      }
+      else
+      {
+        http.get(cb[k], function(res){
+          var data = '';
+          res.on('data', function (chunk){
+            data += chunk;
+          });
+          res.on('error', function(chunk){
+           data += "error";
+          });
+          res.on('end',function(){
+           response.write(data);
+           if(k+1!=j){
+            response.write(', ');
+            }
+            reck(k+1);
+            });
+          
+        }); 
+      }
+    }
+  }
 }).listen(8888);
+
